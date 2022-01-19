c52b5069862680d7b2d5b2b7919ffbbefbe85c69
emily938077 <emily938077@gmail.com>
2022-01-19 16:53:07 +0800

    angular_start finished

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -9,7 +9,20 @@ import { FormBuilder } from '@angular/forms';
 })
 export class CartComponent implements OnInit {
   items = this.cartService.getItems();
-  constructor(private cartService: CartService) {}
+  // checkoutForm 屬性設定為一個包含 name 和 address 欄位的表單模型
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
 
+  onSubmit(): void {
+    //提交後(購買) 購物車內就不會有東西了
+    this.items = this.cartService.clearCart();
+    console.warn('Your ouder has been submitted', this.checkoutForm.reset());
+  }
   ngOnInit(): void {}
 }
